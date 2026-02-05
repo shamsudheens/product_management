60d784f944a8ef6940233c2a9817d90b96f2f494
chechu776 <shameerchechu@gmail.com>
2026-02-05 11:06:48 +0530

    done node app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-require('./dist/app.js');
+const app = require('./dist/app.js').default;
+const PORT = process.env.PORT || 3006;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
